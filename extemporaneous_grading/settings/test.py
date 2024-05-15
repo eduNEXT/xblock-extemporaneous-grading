83258aa9d54bafe31400fc160879d356430fa32a
Bryann Valderrama <64033729+BryanttV@users.noreply.github.com>
2024-05-15 13:34:07 -0500

    chore: set up xblock (#1)

--- extemporaneous_grading/settings/test.py
+++ extemporaneous_grading/settings/test.py
@@ -6,25 +6,26 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+
 import os
 
 BASE_DIR = os.path.dirname(__file__)
 
-SECRET_KEY = os.getenv('DJANGO_SECRET', 'open_secret')
+SECRET_KEY = os.getenv("DJANGO_SECRET", "open_secret")
 
 # Application definition
 
 INSTALLED_APPS = (
-    'statici18n',
-    'extemporaneous_grading',
+    "statici18n",
+    "extemporaneous_grading",
 )
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -36,17 +37,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 # statici18n
 # https://django-statici18n.readthedocs.io/en/latest/settings.html
 
-LOCALE_PATHS = [os.path.join(BASE_DIR, 'extemporaneous_grading', 'conf', 'locale')]
+LOCALE_PATHS = [os.path.join(BASE_DIR, "extemporaneous_grading", "conf", "locale")]
 
-STATICI18N_DOMAIN = 'text'
-STATICI18N_NAMESPACE = 'ExtemporaneousGradingI18n'
-STATICI18N_PACKAGES = (
-    'extemporaneous_grading',
-)
-STATICI18N_ROOT = 'extemporaneous_grading/public/js'
-STATICI18N_OUTPUT_DIR = 'translations'
+LANGUAGES = [
+    ("en", "English"),
+    ("es_ES", "Spanish (Spain)"),
+    ("es_419", "Spanish (Latin America)"),
+]
+
+STATICI18N_DOMAIN = "text"
+STATICI18N_NAMESPACE = "ExtemporaneousGradingI18n"
+STATICI18N_PACKAGES = ("extemporaneous_grading",)
+STATICI18N_ROOT = "extemporaneous_grading/public/js"
+STATICI18N_OUTPUT_DIR = "translations"
---
+++ extemporaneous_grading/utils.py
@@ -0,0 +1,10 @@
+"""
+Utilities for Extemporaneous Grading XBlock.
+"""
+
+
+def _(text):
+    """
+    Make '_' a no-op so we can scrape strings.
+    """
+    return text
