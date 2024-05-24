dea4544e1fbd596ef5362b3b0727ac1e96263610
Bryann Valderrama <64033729+BryanttV@users.noreply.github.com>
2024-05-24 12:24:49 -0500

    feat: allow instructors download late submissions csv file (#5)

---
+++ extemporaneous_grading/constants.py
@@ -0,0 +1,6 @@
+"""Constants for the Extemporaneous Grading XBlock."""
+
+ATTR_KEY_USER_ROLE = "edx-platform.user_role"
+ATTR_ANONYMOUS_USER_ID = "edx-platform.anonymous_user_id"
+ATTR_USER_USERNAME = "edx-platform.username"
+TIME_PATTERN = r"^([01][0-9]|2[0-3]):[0-5][0-9]$"
